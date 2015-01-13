b8099d506018e4a9bddf6072d5d4817abf7cd85b
Dale Naho'olewa <dnahoole@hawaii.edu>
2015-01-13 09:20:22 +0000

    dynamic section sizes

--- web/js/basic.js
+++ web/js/basic.js
@@ -2,6 +2,9 @@
 const MELT = 'sect1';
 
 function init() {
+    document.getElementById('layer1').addEventListener('click', showLayer, false);
+    document.getElementById('layer2').addEventListener('click', showLayer, false);
+    document.getElementById('layer3').addEventListener('click', showLayer, false);
 }
 
 function mouseOver() {
@@ -19,11 +22,33 @@ function changeText() {
 
 function changeMusic(urlMusic)
 {
-// alert(urlMusic);
    document.getElementById("mySel").setAttribute("src", urlMusic); 
    document.getElementById("myMusic").load();
    document.getElementById("myMusic").play();
 }
 
+function showLayer(e) {
+    var layer1 = document.getElementById('layer1');
+    var layer2 = document.getElementById('layer2');
+    var layer3 = document.getElementById('layer3');
+
+    var youtube = document.getElementById('youtube');
+    var cube = document.getElementById('cube');
+    var ocean = document.getElementById('ocean');
+
+    youtube.setAttribute("style","height: 0px");
+    cube.setAttribute("style","height: 0px");
+    ocean.setAttribute("style","height: 0px");
+
+    if (e.target === layer1) {
+        youtube.setAttribute("style","height: 400px");
+    } else if (e.target === layer2) {
+        cube.setAttribute("style","height: 400px");
+    } else if (e.target === layer3) {
+        ocean.setAttribute("style","height: 400px");
+        if (ocean.getAttribute("src") === "") 
+            ocean.setAttribute("src","adv/ocean.html");
+   }
+}
 
 window.addEventListener("load", init, false);
